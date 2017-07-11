ff1c76bcbfa7450b180392fa4fc356336c11cb1e
Jieli Chen <chenjieli_1984@163.com>
2017-07-11 11:39:19 -0700

    Create LabeledLexer.java

---
+++ LabeledLexer.java
@@ -0,0 +1,10 @@
+import org.antlr.v4.runtime.CharStream;
+
+public class LabeledLexer extends ImprovedLexer {
+
+	public LabeledLexer(CharStream input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+}
