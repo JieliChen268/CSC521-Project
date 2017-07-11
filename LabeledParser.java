6ec474ccc7db83a1e84495179f43a983e3b7bb1d
Jieli Chen <chenjieli_1984@163.com>
2017-07-11 11:39:59 -0700

    Create LabeledParser.java

---
+++ LabeledParser.java
@@ -0,0 +1,10 @@
+import org.antlr.v4.runtime.TokenStream;
+
+public class LabeledParser extends ImprovedParser {
+
+	public LabeledParser(TokenStream input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+}
